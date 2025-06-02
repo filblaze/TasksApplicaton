28e256d15d47923455e375a7e517965ba6689c2d
Filip Błażewicz <filipblazewicz@gmail.com>
2025-06-02 22:04:27 +0200

    First commit.

---
+++ src/main/java/com/kodilla/tasks/domain/Task.java
@@ -0,0 +1,12 @@
+package com.kodilla.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class Task {
+    private Long id;
+    private String title;
+    private String content;
+}
